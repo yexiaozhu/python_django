fd96988c05d4ace320946868ec04b0626d7acf30
557703988@139.com <557703988@139.com>
2017-07-06 01:00:49 -0700

    Writing your first Django app, part 2<introducing the django admin>

--- myweb/myweb/settings.py
+++ myweb/myweb/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'leaf',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,16 @@ WSGI_APPLICATION = 'myweb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'web_data',
+        'USER': 'ubuntu',
+        'PASSWORD': 'w123456ub',
+        'HOST': 'localhost',
+        'PORT': 3306,
     }
 }
 
