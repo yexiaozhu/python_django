473f7ab36af31feada35a0e66f718cb54968648f
557703988@139.com <557703988@139.com>
2017-07-06 19:40:18 -0700

    Writing your first Django app, part 3<use the template system>

--- myweb/myweb/settings.py
+++ myweb/myweb/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'myweb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates/').replace('\\', '/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
